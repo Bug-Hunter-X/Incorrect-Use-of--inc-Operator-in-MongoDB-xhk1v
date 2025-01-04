3df452cb9c4658c19734ca3ba0bb3317a840808c
BugHunter-X <askzenai@gmail.com>
2025-01-04 18:46:03 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+//Incorrect use of $inc operator
+db.collection.updateOne({name:"John"},{$inc:{age:100}});
+//Correct use of $inc operator
+db.collection.updateOne({name:"John"},{$inc:{age:1}});
+```
